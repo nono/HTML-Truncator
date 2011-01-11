0173e6bc80306e3972bfa89da61693d8a9078305
Bruno Michel <bmichel@menfin.info>
2011-01-11 22:37:04 +0100

    It works like I want (but françois does not agree)

--- lib/html_truncator.rb
+++ lib/html_truncator.rb
@@ -4,34 +4,44 @@ require "nokogiri"
 class HTML_Truncator
   def self.truncate(text, max_words, ellipsis="...")
     doc = Nokogiri::HTML::DocumentFragment.parse(text)
-    doc.truncate(max_words, ellipsis)
+    doc.truncate(max_words, ellipsis).first
   end
 end
 
 
 class Nokogiri::HTML::DocumentFragment
-  def truncate(max_words, ellipsis)
-    inner_truncate(max_words, ellipsis).first
+  def ellipsable?
+    true
   end
 end
 
 class Nokogiri::XML::Node
   def truncate(max_words, ellipsis)
-    inner, remaining = inner_truncate(max_words, ellipsis)
+    inner, remaining, ellipsed = inner_truncate(max_words, ellipsis)
     children.remove
     add_child Nokogiri::HTML::DocumentFragment.parse(inner)
-    [to_xml, max_words - remaining]
+    [to_xml, max_words - remaining, ellipsed]
   end
 
   def inner_truncate(max_words, ellipsis)
-    inner, remaining = "", max_words
+    inner, remaining, ellipsed = "", max_words, false
     self.children.each do |node|
-      txt, nb = node.truncate(remaining, ellipsis)
+      txt, nb, ellipsed = node.truncate(remaining, ellipsis)
       remaining -= nb
-      inner += txt
-      break if remaining < 0
+      inner += txt if node.text? || node.ellipsable? || node.content.split.length != 0
+      if remaining < 0
+        if !ellipsed && ellipsable?
+          inner += ellipsis
+          ellipsed = true
+        end
+        break
+      end
     end
-    [inner, remaining]
+    [inner, remaining, ellipsed]
+  end
+
+  def ellipsable?
+    %w(p ol ul li div header article nav section footer aside dd dt dl).include? name
   end
 end
 
@@ -39,8 +49,7 @@ class Nokogiri::XML::Text
   def truncate(max_words, ellipsis)
     words    = content.split
     nb_words = words.length
-    return [to_xhtml, nb_words] if nb_words <= max_words
-    return [ellipsis, 1] if max_words == 0
-    [words.slice(0, max_words).join(' ') + ellipsis, nb_words]
+    return [to_xhtml, nb_words, false] if nb_words <= max_words
+    [words.slice(0, max_words).join(' '), nb_words, false]
   end
 end
--- spec/html_truncator_spec.rb
+++ spec/html_truncator_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 path = File.expand_path(File.dirname(__FILE__) + "/../lib/")
 $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
 require "html_truncator"
@@ -13,7 +14,7 @@ describe HTML_Truncator do
   end
 
   it "should truncate long text to the given number of words" do
-    words = HTML_Truncator.truncate(long_text, 10, "").gsub(/<[^>]*>/, '').split
+    words = HTML_Truncator.truncate(long_text, 10, "").gsub(/<[^>]*>/, ' ').split
     words.should have(10).items
     words = HTML_Truncator.truncate(long_text, 11, "").gsub(/<[^>]*>/, '').split
     words.should have(11).items
@@ -31,17 +32,26 @@ describe HTML_Truncator do
   end
 
   it "should truncate long text" do
-    HTML_Truncator.truncate(long_text, 3, "...").should == "<p>Foo <b>Bar Baz</b> <b>...</b></p>"
-    HTML_Truncator.truncate(long_text, 4, "...").should == "<p>Foo <b>Bar Baz</b> <b>Bar...</b></p>"
+    HTML_Truncator.truncate(long_text, 3, "...").should == "<p>Foo <b>Bar Baz</b> ...</p>"
+    HTML_Truncator.truncate(long_text, 4, "...").should == "<p>Foo <b>Bar Baz</b> <b>Bar</b>...</p>"
     HTML_Truncator.truncate(list_text, 3, "...").should == "<p>Foo:</p><ul><li>Bar Baz</li> <li>...</li></ul>"
     HTML_Truncator.truncate(list_text, 4, "...").should == "<p>Foo:</p><ul><li>Bar Baz</li> <li>Bar...</li></ul>"
   end
 
   it "should be possible to truncate with HTML in the ellipsis" do
-    HTML_Truncator.truncate(long_text, 2, ' <a href="/more">...</a>').should == '<p>Foo <b>Bar <a href="/more">...</a></b></p>'
+    HTML_Truncator.truncate(long_text, 2, ' <a href="/more">...</a>').should == '<p>Foo <b>Bar</b> <a href="/more">...</a></p>'
   end
 
   it "should preserve spaces inside a node" do
     HTML_Truncator.truncate("<p>bla bla bla bla bla</p>", 2, "...").should == "<p>bla bla...</p>"
+  end
+
+  it "should not bug on pre" do
+    HTML_Truncator.truncate("<p>foo bar</p><pre>foo bar</pre>", 3, "...").should == "<p>foo bar</p><pre>foo</pre>..."
+  end
+
+  it "should not bug on françois test" do
+    HTML_Truncator.truncate("<p>foo bar</p><pre>foo bar</pre>plop", 3, "...").should == "<p>foo bar</p><pre>foo</pre>..."
+    HTML_Truncator.truncate("<p>Foo <b>Bar Baz</b> plop<b>Foo Bar</b></p>", 3, "...").should == "<p>Foo <b>Bar Baz</b>...</p>"
   end
 end
